4a8f4f219ab1c330b009523685a5ee3e4f49f313
gamer6462 <118704712+gamer6462@users.noreply.github.com>
2023-12-29 09:40:53 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,31 @@
+"""Creating a Comment
+Comments starts with a #, and Python will ignore them:"""
+#This is a comment
+print("Welcome To tech_nest")
+
+#Example
+print("Hello, World!") #This is a comment 
+"""Comments can be placed at the end of a line, and Python will ignore the rest of the line:"""
+
+#print("Hello, World!")
+print("Cheers, Mate!") 
+"""A comment does not have to be text that explains the code, it can also be used to prevent Python from executing code:"""
+
+#Multiline Comments
+"""Python does not really have a syntax for multiline comments.
+To add a multiline comment you could insert a # for each line:"""
+#This is a comment
+#written in
+#more than just one line
+print("Hope U understood the concept of commnets")
+
+"""Or, not quite as intended, you can use a multiline string.
+Since Python will ignore string literals that are not assigned to a variable, you can add a multiline string (triple quotes) in your code, and place your comment inside it:"""
+
+#Example
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Subscribe To tech_nest") 
---
+++ day3.py
@@ -0,0 +1,50 @@
+"""Built-in Data Types
+
+In programming, data type is an important concept.
+Variables can store data of different types, and different types can do different things.
+Python has the following data types built-in by default, in these categories:
+
+Text Type: 	str
+Numeric Types: 	int, float, complex
+Sequence Types: list, tuple, range
+Mapping Type: 	dict
+Set Types:  	set, frozenset
+Boolean Type: 	bool
+Binary Types: 	bytes, bytearray, memoryview
+None Type:   	NoneType
+"""
+
+
+#str 
+x1 = "Hello World"
+#display x:
+print(x1)
+#display the data type of x:
+print(type(x1))
+
+
+
+#int
+x = 20
+#display x:
+print(x)
+#display the data type of x:
+print(type(x))
+
+
+
+#float
+a=20.3
+print(a)
+print(type(a))
+
+
+
+#bool
+a2=True
+print(a2)
+print(type(a2))
+
+b=False
+print(b)
+print(type(b))
